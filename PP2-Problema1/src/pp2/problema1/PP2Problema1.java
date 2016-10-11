84d888f204fc983f92281ba2a6af2169df4bf872
Rriverag98 <rriverag98@gmail.com>
2016-10-11 09:01:40 -0500

    FP-PP2-Problema 1. Escribir un programa que determine si un año es bisiesto. Una año es bisiesto si el múltiplo de 4. • (Ej. 1984. Los años múltiplos de 100 no son bisiestos, salvo si ellos son también múltiplos de 400 (2000 es bisiesto, 1800 no lo es).

---
+++ PP2-Problema1/src/pp2/problema1/PP2Problema1.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema1;
+import java.util.*;
+/**
+ *
+ * @author Rodrigo
+ */
+public class PP2Problema1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int Anyo;
+    Anyo = Numero();
+    calcular(Anyo);
+    }
+    public static void calcular(int Anyo){ 
+    if ((Anyo%4)==0){
+    //AñoBisiesto();    
+    } else {AnyoNormal();
+    }
+    if ((Anyo%100)==0){
+     //AñoNormal();   
+    } else {AnyoBisiesto();
+    }
+    if ((Anyo%400)==0){
+      AnyoBisiesto();   
+    } else {AnyoNormal();  
+    }
+    }
+    public static int Numero(){
+    System.out.println("Introduce el año: ");
+    int Dato;
+    Scanner teclado = new Scanner(System.in);
+    Dato = teclado.nextInt();
+    return Dato;    
+    }
+    public static void AnyoNormal(){
+     System.out.println("El año no es bisiesto");
+     System.exit(0);
+    }
+    public static void AnyoBisiesto(){
+        System.out.println("El año es bisiesto"); 
+        System.exit(0);
+    }
+    
+}
