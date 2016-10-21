ece5f4d6a47e68f05e668b0d88f7ca375c54d666
Rriverag98 <rriverag98@gmail.com>
2016-10-20 23:49:28 -0500

    6.	Realizar un programa capaz de recibir las horas trabajadas de un empleado y la paga por hora para calcular el sueldo semanal de un empleado . Cuando la horas de trabajo exceden de 40 se consideran horas extra; las primeras 8 se pagan al doble; si las horas extra exceden de 8 se pagan las primeras 8 al doble de lo que se pagan las horas normales y el resto al triple..

--- PP2-Problema6/src/pp2/problema6/PP2Problema6.java
+++ PP2-Problema6/src/pp2/problema6/PP2Problema6.java
@@ -18,7 +18,7 @@ public class PP2Problema6 {
      */
      public static void main(String[] args) {
     int horas;
-    double resultado1,resultado2,resultado3;
+    double resultado1;
     horas=duracion();
     resultado1=calcular1(horas);
     mostrarresultado(horas,resultado1);
@@ -31,13 +31,13 @@ public class PP2Problema6 {
     return dato;    
     }
     public static double calcular1(int horas){
-    if (horas>40&&horas<48){
-    return (((horas-40)*18.25)+40*9.125);    
+    if (horas>40&&horas<48){ //Si las horas trabajadas son mayor a 40 y menor a 48 
+    return (((horas-40)*18.25)+40*9.125);//restarle 40 a las horas y multiplicar por el doble del salario normal, despues se le suma el salario por las 40 horas
     } 
-    if (horas>48){
-    return (((horas-48)*27.375)+8*18.25+40*9.125);        
+    if (horas>48){ //Si las horas trabajadas son mayor a 48
+    return (((horas-48)*27.375)+8*18.25+40*9.125);//restarle 48 a las horas y multiplicar por el triple del salario normal, despues se le suma el salario por las 40 horas de salario normal y la 8 horas  de salario doble  
     }
-    return (horas*9.125);
+    return (horas*9.125);//Si las horas trabajadas son menor o igual a 40 
     }
     public static void mostrarresultado(int horas,double resultado1){
     System.out.println(horas +" horas es igual a: "+resultado1+" MXP");
