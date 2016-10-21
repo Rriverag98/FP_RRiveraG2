fb75c3fdf608c0437ba4a059d85a87482b701e19
Rriverag98 <rriverag98@gmail.com>
2016-10-21 01:05:44 -0500

    18.	Realizar un programa que contenga una función que calcule el mínimo común múltiplo de tres números

--- PP2-Problema18/src/pp2/problema18/PP2Problema18.java
+++ PP2-Problema18/src/pp2/problema18/PP2Problema18.java
@@ -34,14 +34,27 @@ public class PP2Problema18 {
    public static int calcular(int a, int b,int c){
         int r=1,n;
         n=a;
-        if(n<b)n=b;
+        if(n<b)n=b; //se busca el menor de los 3 numeros
         if(n<c)n=c;
         for (int i=2;i<n;i++){
-            if (a%i==0||b%i==0||c%i==0){
-                r=r*i;
-                if (a%i==0) a=a/i;
-                if (b%i==0) b=b/i;
-                if (c%i==0) c=c/i;
+            int p=0;
+            if (a%i==0||b%i==0||c%i==0){//busca un numero que divida a los 3 si dejar remainders
+                if (a%i==0){
+                    int p1=0;
+                    do {a=a/i;p1++;}
+                    while(a%i==0);
+                    p=p1;}//aumenta p hasta que el numero termine el loop y guarda el valor de i
+                if (b%i==0){
+                    int p2=0;
+                    do {b=b/i;p2++;}
+                    while(b%i==0);
+                    if(p2>p)p=p2;} //aumenta p2 hasta que el numero termine el loop y en caso de ser mayor a p se guarda el valor de p2 en lugar de p1
+                if (c%i==0){
+                    int p3=0;
+                    do {c=c/i;p3++;}
+                    while(c%i==0);
+                    if(p3>p)p=p3;}//aumenta p3 hasta que el numero termine el loop y en caso de ser mayor a p se guarda el valor de p3 en lugar de p2
+                r=(int) (r*(Math.pow(i, p)));//guarda el producto de los divisores
             }
         }
         return r;
