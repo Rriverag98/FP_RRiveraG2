fb19790915cc547f700674de8a4e79b4bf143854
Rriverag98 <rriverag98@gmail.com>
2016-10-20 23:26:10 -0500

    FP-PP2-Problema 16.	Realizar un programa que contenga una función que reciba como parámetros tres números y que devuelva y muestre el mayor de ellos.

---
+++ PP2-Problema16/src/pp2/problema16/PP2Problema16.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema16;
+import java.util.*;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class PP2Problema16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            int n,a,b; 
+        n=pedirnumero("A");
+        a=pedirnumero("B");
+        b=pedirnumero("C");
+        resultado(n,a,b);
+         }
+    public static int pedirnumero(String x){
+    int n;
+    System.out.println("Introduce el numero " + x +" : ");
+    Scanner teclado = new Scanner(System.in);
+    n=teclado.nextInt();
+    return  n;
+    }
+    public static void resultado(int n,int a,int b){
+        if (b<n&&a<n){
+            System.out.println("El numero mas grande es: "+n);
+        }
+         if (b<a&&n<a){
+            System.out.println("El numero mas grande es: "+a);
+        }
+          if (n<b&&a<b){
+            System.out.println("El numero mas grande es: "+b);
+        }
+         }
+}
+    
+   
