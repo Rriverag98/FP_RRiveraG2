1da5cca52a806918ff1657973c166651a7918b3f
Rriverag98 <rriverag98@gmail.com>
2016-10-20 23:23:48 -0500

    FP-PP2-Problema 12.	Realiza un programa que reciba 10 números enteros por teclado y guardarlos en un arreglo, después calcula y muestra la media de los números que estén en las posiciones pares del arreglo.

---
+++ PP2-Problema12/src/pp2/problema12/PP2Problema12.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema12;
+import java.util.*;
+/**
+ *
+ * @author Rodrigo
+ */
+public class PP2Problema12 {
+public static void main(String[] args) {
+        int [] n = new int [10];
+        n=pedirnumeros(n);
+        Muestranumeros(n);
+    }
+    public static int [] pedirnumeros(int [] n){
+ Scanner teclado = new Scanner(System.in);
+ for (int i=0;i<n.length;i++){
+ System.out.print("Ingresa el numero en la pocision: " + (i+1) + " " ); 
+    n [i]=teclado.nextInt();
+    } return  n;
+    }
+    public static void Muestranumeros(int[] n){
+           int a=0,p=0;
+        for (int i=1; i<n.length; i=i+1){
+            a=n[i]+a;
+            p++;
+        }
+        System.out.println("La media de los numeros introducidos es: "+ a/p);
+    }
+}
+ 
+    
+
