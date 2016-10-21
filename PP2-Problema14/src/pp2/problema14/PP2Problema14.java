1b34ac586d6ec7513c31b4d3e04c73dcc4f6331d
Rriverag98 <rriverag98@gmail.com>
2016-10-21 00:27:27 -0500

    14.	Crear una tabla de tamaño 7x7 y rellenarla de forma que los elementos de la diagonal principal sean 1 y el resto 0.

--- PP2-Problema14/src/pp2/problema14/PP2Problema14.java
+++ PP2-Problema14/src/pp2/problema14/PP2Problema14.java
@@ -15,17 +15,17 @@ public class PP2Problema14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int [][] tabla = new int [7][7];
+       int [][] tabla = new int [7][7]; //vector 2d para generar una tabla 7x7
        tabla = llenar(tabla);
        ejecutar(tabla);
     }
     public static int [][] llenar(int [][] tabla){
           for(int i=0;i<tabla.length;i++){
             for(int j=0;j<tabla.length;j++){
-                if(i==j){
-                    tabla[i][j]=1;
+                if(i==j){  
+                    tabla[i][j]=1; //inserta 1 cuando los valores i y j son iguales
                 } else {
-                    tabla[i][j]=0;
+                    tabla[i][j]=0; //Si los valores de i y j no son los mismos, inserta 0s 
                 }
             }
         }
